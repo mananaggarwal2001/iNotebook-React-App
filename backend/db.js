e37a930837692e8178839f429f0ad69ecc37e028
AnonymousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2022-09-05 02:21:18 +0530

    changes amde and added the db.js file.

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?directConnection=true";
+const connetToMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log('connection made successfully and the mongoose is working properly');
+    });
+}
+
+module.exports= connetToMongo;
