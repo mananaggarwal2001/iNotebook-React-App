f560b2ba7a85824aa3d1dc28811f73471124bf6e
AnonymousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2022-10-12 21:26:11 +0530

    Changes made and added the note.js new component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -11,16 +11,36 @@ const Notes = () => {
 
     useEffect(() => {
         getNotes();
-    },[])
+    }, []);
+
+
+
 
     return (
         <>
             <AddNote />
+            <div className="modal" tabindex="-1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Edit Note</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="row my-3">
 
                 <h3>Your Notes</h3>
                 {notes.map((note) => {
-                    return <NoteItem key={note._id} note={note} />;
+                    return <NoteItem key={note._id} note={note} updateNote= {updateNote} />;
                 })}
             </div>
         </>
